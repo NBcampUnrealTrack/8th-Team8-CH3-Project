1e68613e7b4551e60b57c68a7426a17496683a67
markpark65 <jhmark65@naver.com>
2026-05-09 01:10:25 +0900

    Update interacting door

---
+++ Source/Oblivio/Public/DoorBase.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DoorBase.generated.h"
+
+UCLASS()
+class OBLIVIO_API ADoorBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ADoorBase();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Interaction")
+	void InteractDoor();
+
+	// 다음 레벨의 이름을 저장하는 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level Transition")
+	FName NextLevelName;
+
+	// 이 문이 레벨을 이동시키는 문인지 체크하는 플래그
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level Transition")
+	bool bIsExitDoor = false;
+};
